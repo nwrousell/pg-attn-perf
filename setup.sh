2cf3c072afac2b0a40d509f729bc1cb2795ca481
Noah Rousell <noah@rousell.org>
2026-05-02 13:44:29 -0400

    faster setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+# first grab a beefy interactive gpu node with `interact -q gpu -g 1 -n 12 -m 40g -t 4:00:00`
+
 module load cuda/12.9
 uv venv --python 3.12 --seed
 source .venv/bin/activate
-uv pip install vllm --torch-backend=auto -v
+
+# these make the build run faster
+export MAX_JOBS=12
+export VLLM_USE_PRECOMPILED=1
+
+uv pip install vllm --torch-backend=cu129 -v
